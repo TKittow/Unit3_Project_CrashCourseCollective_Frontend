af9f02673d4599ced6055d527cc63ba09b923aeb
kiwicj <cam.orr93@gmail.com>
2024-02-02 11:49:35 +0000

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+
+const CLIENT_ID = '18b849ea0dd132f6729a'
+
 
 function App() {
+
+useEffect(()=> {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString) 
+  const codeParam = urlParams.get('code')
+  console.log(codeParam);
+}, []) // empty array to make the use effect only run once
+
+  function gitHubLogin(){ 
+    window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={gitHubLogin}>Sign in with Git</button>
     </div>
   );
 }
 
 export default App;
+
+
+// cd648d801403bd594f2c
