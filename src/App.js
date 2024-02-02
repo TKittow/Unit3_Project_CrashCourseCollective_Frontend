053adc1b13cb83e847d84e1c9d13df4b3a56c796
kiwicj <cam.orr93@gmail.com>
2024-02-02 20:40:30 +0000

    GitHub OAuth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import HomePage from './pages/HomePage/HomePage'
-import AboutPage from './pages/AboutPage/AboutPage'
-import CohortPage from './pages/CohortPage/CohortPage';
-import { Routes, Route } from 'react-router-dom'
-import { useEffect, useState, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const CLIENT_ID = '18b849ea0dd132f6729a'
 // Need to remove the above, as it should be in the backend,
-// Front end doesnt like to pull through from an import?
+// Front end doesnt like to pull through from an import
 
 function App() {
+  const [rerender, setRerender] = useState(false)
+  const [userData, setUserData] = useState({})
+
+
 
 useEffect(() => {
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString) 
   const codeParam = urlParams.get('code')
-  console.log(codeParam);
+  
+  if(codeParam && !localStorage.getItem('accessToken')) {
+    async function getAccessToken() {
+      await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
+        method:'GET'
+      }).then ((response) => {
+        return response.json()
+      }).then((data) =>{
+        console.log(data);
+        if(data.access_token) {
+          localStorage.setItem('accessToken', data.access_token) // setItem does not force a rerender on react. We want it to so we can show a state with the user being logged in
+          setRerender(!rerender)
+        }
+      })
+    }
+    getAccessToken()
+  }
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, []) // empty array to make the use effect only run once
 
 async function getUserData() {
@@ -41,7 +57,40 @@ async function getUserData() {
 
   return (
     <div className="App">
-      <button onClick={gitHubLogin}>Sign in with Git</button>
+      <main>
+        <div className='login'>
+          {localStorage.getItem('accessToken') ?
+            <>
+              {/* //insert if logged in items  */}
+              <h1>We have a token</h1>
+              <button onClick={() => { localStorage.removeItem('accessToken'); setRerender(!rerender)}}>
+                Log Out
+              </button>
+              <h3> Get user data </h3>
+              <button onClick={getUserData}>Get Data</button>
+              {Object.keys(userData).length !== 0 ?
+              <>
+                <h4> Hey there {userData.login}</h4>
+                <img width='100px' height='100px' src={userData.avatar_url} alt='profile pic'></img>
+                <a href={userData.html_url} style={{'color' : 'blue'}}>Link to GitHub profile</a>
+              </>
+              :
+              <>
+              </>}
+            </>
+          :
+            <>
+            <button onClick={gitHubLogin}>
+              Sign in with Git
+            </button>
+            </>
+          }
+        </div>
+        <div className='projectGrid'>
+          {/* // display public projects for non logged in users */}
+        </div>
+        
+      </main>
     </div>
   );
 }
