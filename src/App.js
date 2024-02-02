cc0565a94227683722d010cb331d260c16184621
kiwicj <cam.orr93@gmail.com>
2024-02-02 16:26:12 +0000

    WIP Git OAuth

--- src/App.js
+++ src/App.js
@@ -8,43 +8,43 @@ function App() {
   const [rerender, setRerender] = useState(false)
   const [userData, setUserData] = useState({})
 
-useEffect(()=> {
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString) 
-  const codeParam = urlParams.get('code')
+// useEffect(()=> {
+//   const queryString = window.location.search
+//   const urlParams = new URLSearchParams(queryString) 
+//   const codeParam = urlParams.get('code')
   
-  if(codeParam && (localStorage.getItem('accessToken') === null)) {
-    async function getAccessToken() {
-      await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
-        method:'GET'
-      }).then ((response) => {
-        return response.json()
-      }).then((data) =>{
-        console.log(data);
-        if(data.access_token) {
-          localStorage.setItem('accessToken', data.access_token) // setItem does not force a rerender on react. We want it to so we can show a state with the user being logged in
-          setRerender(!rerender)
-        }
-      })
-    }
-    getAccessToken()
-  }
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, []) // empty array to make the use effect only run once
+//   if(codeParam && (localStorage.getItem('accessToken') === null)) {
+//     // async function getAccessToken() {
+//     //   await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
+//     //     method:'GET'
+//     //   }).then ((response) => {
+//     //     return response.json()
+//     //   }).then((data) =>{
+//     //     console.log(data);
+//     //     if(data.access_token) {
+//     //       localStorage.setItem('accessToken', data.access_token) // setItem does not force a rerender on react. We want it to so we can show a state with the user being logged in
+//     //       setRerender(!rerender)
+//     //     }
+//     //   })
+//     }
+//     getAccessToken()
+//   }
+// // eslint-disable-next-line react-hooks/exhaustive-deps
+// }, []) // empty array to make the use effect only run once
 
-async function getUserData() {
-  await fetch('http://localhost:4000/getUserData', {
-    method: 'GET',
-    headers: {
-      'Authorization' : 'Bearer ' + localStorage.getItem('accessToken')
-    }
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    setUserData(data)
-  })
-}
+// //async function getUserData() {
+// //   await fetch('http://localhost:4000/getUserData', {
+// //     method: 'GET',
+// //     headers: {
+// //       'Authorization' : 'Bearer ' + localStorage.getItem('accessToken')
+// //     }
+// //   }).then((response) => {
+// //     return response.json()
+// //   }).then((data) => {
+// //     console.log(data)
+// //     setUserData(data)
+// //   })
+// // }
 
   function gitHubLogin(){ 
     window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID)
@@ -64,7 +64,7 @@ async function getUserData() {
                 Log Out
               </button>
               <h3> Get user data </h3>
-              <button onClick={getUserData}>Get Data</button>
+              {/* <button onClick={getUserData}>Get Data</button> */}
               {Object.keys(userData).length !== 0 ?
               <>
                 <h4> Hey there {userData.login}</h4>
@@ -86,6 +86,7 @@ async function getUserData() {
         <div className='projectGrid'>
           {/* // display public projects for non logged in users */}
         </div>
+        
       </main>
     </div>
   );
