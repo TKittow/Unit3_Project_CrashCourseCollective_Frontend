612ce3ca45631de09862ccf4331e95eda5d2b52b
kiwicj <cam.orr93@gmail.com>
2024-02-02 18:58:48 +0000

    working Git OAuth

--- src/App.js
+++ src/App.js
@@ -7,49 +7,51 @@ import { Routes, Route } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
 const CLIENT_ID = '18b849ea0dd132f6729a'
-
+// Need to remove the above, as it should be in the backend,
+// Front end doesnt like to pull through from an import?
 
 function App() {
   const [rerender, setRerender] = useState(false)
   const [userData, setUserData] = useState({})
 
-// useEffect(()=> {
-//   const queryString = window.location.search
-//   const urlParams = new URLSearchParams(queryString) 
-//   const codeParam = urlParams.get('code')
-  
-//   if(codeParam && (localStorage.getItem('accessToken') === null)) {
-//     // async function getAccessToken() {
-//     //   await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
-//     //     method:'GET'
-//     //   }).then ((response) => {
-//     //     return response.json()
-//     //   }).then((data) =>{
-//     //     console.log(data);
-//     //     if(data.access_token) {
-//     //       localStorage.setItem('accessToken', data.access_token) // setItem does not force a rerender on react. We want it to so we can show a state with the user being logged in
-//     //       setRerender(!rerender)
-//     //     }
-//     //   })
-//     }
-//     getAccessToken()
-//   }
-// // eslint-disable-next-line react-hooks/exhaustive-deps
-// }, []) // empty array to make the use effect only run once
 
-// //async function getUserData() {
-// //   await fetch('http://localhost:4000/getUserData', {
-// //     method: 'GET',
-// //     headers: {
-// //       'Authorization' : 'Bearer ' + localStorage.getItem('accessToken')
-// //     }
-// //   }).then((response) => {
-// //     return response.json()
-// //   }).then((data) => {
-// //     console.log(data)
-// //     setUserData(data)
-// //   })
-// // }
+useEffect(() => {
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString) 
+  const codeParam = urlParams.get('code')
+  
+  if(codeParam && !localStorage.getItem('accessToken')) {
+    async function getAccessToken() {
+      await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`, {
+        method:'GET'
+      }).then ((response) => {
+        return response.json()
+      }).then((data) =>{
+        console.log(data);
+        if(data.access_token) {
+          localStorage.setItem('accessToken', data.access_token) // setItem does not force a rerender on react. We want it to so we can show a state with the user being logged in
+          setRerender(!rerender)
+        }
+      })
+    }
+    getAccessToken()
+  }
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, []) // empty array to make the use effect only run once
+
+async function getUserData() {
+  await fetch('http://localhost:4000/getUserData', {
+    method: 'GET',
+    headers: {
+      'Authorization' : 'Bearer ' + localStorage.getItem('accessToken')
+    }
+  }).then((response) => {
+    return response.json()
+  }).then((data) => {
+    console.log(data)
+    setUserData(data)
+  })
+}
 
   function gitHubLogin(){ 
     window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID)
@@ -75,7 +77,7 @@ function App() {
                 Log Out
               </button>
               <h3> Get user data </h3>
-              {/* <button onClick={getUserData}>Get Data</button> */}
+              <button onClick={getUserData}>Get Data</button>
               {Object.keys(userData).length !== 0 ?
               <>
                 <h4> Hey there {userData.login}</h4>
