ab290fad141f15e0e904a46ec296e6364244cee2
kiwicj <cam.orr93@gmail.com>
2024-02-05 11:13:16 +0000

    Profile page WIP

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,22 +1,31 @@
 //! Imports
-// import Navbar from ./src/components/NavBar/NavBar
+import { useState } from 'react'
+import Navbar from './src/components/NavBar/NavBar'
+import ProjectCard from './src/components/ProjectCard'
+import AddProjectModal from './src/components/AddProjectModal'
 
 export default function ProfilePage( /* {projects, users} */){
 
 
 
-    return (
-        <div className="profilePage">
+return (
+    <div className="profilePage">
+        <Navbar />
+        <div className='userInfo'>
+            <img src={userData.avatar_url} alt="Users Image" className='profileImage'/>
+            <div>
+                <h2>{userData.name}</h2>
+                <p>{userData.info}</p>  {/* we will need to add in an info part so the user can complete. */}
+                <span>{userData.html_url}</span>
+            </div>
+        </div>
 
-        <p>The Profile Page</p>
-        
-
+        <button onClick={openModal}>Add Project</button>
 
         <div className="projectGrid">
-            {/*  */}
-            {/* Project Card Component */}
+            
         </div>
-        </div>
-    )
+    </div>
+)
 
 }
