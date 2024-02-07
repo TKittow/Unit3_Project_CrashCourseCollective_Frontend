f89bb883b90babea5d20f52db28c43968a3fac78
tkittow <toby@kittow.com>
2024-02-07 11:54:53 +0000

    currently calling useffect to populate the projects in many different places

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AboutPage from './pages/AboutPage/AboutPage'
 import CohortPage from './pages/CohortPage/CohortPage'
 import ProfilePage from './pages/ProfilePage/ProfilePage'
 import EditProfilePage from './pages/EditProfilePage/EditProfilePage'
+import ProjectPage from './pages/ProjectPage/ProjectPage';
 import { Routes, Route } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useUsers } from './context/UserContext'
@@ -18,9 +19,10 @@ function App() {
   const [rerender, setRerender] = useState(false)
   const [userData, setUserData] = useState({})
   const { addUser } = useUsers()
-  const { projects, } = useProjects()
+ const { getProjects, projects } = useProjects()
   
   //! 'projects' as above will be moved to the project card
+
 
 useEffect(() => {
   const queryString = window.location.search
@@ -79,6 +81,10 @@ useEffect(() => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
 }, [userData.login])
 
+useEffect(() => {
+  getProjects()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [])
 
 
 
@@ -127,12 +133,13 @@ async function loginUser() {
         </div>
         <hr/>
           <Routes>
-            <Route path='/' element={ <HomePage userData={userData} projects={projects}/> } />
+            <Route path='/' element={ <HomePage userData={userData}/> } />
             <Route path='/about' element={ <AboutPage userData={userData}/> } />
             <Route path='/cohort' element={ <CohortPage /> } />
             <Route path='/profilepage' element={ <ProfilePage userData={userData} /> } />
             <Route path='/editprofilepage' element={ <EditProfilePage userData={userData}/> } />
             <Route path='/login' />
+            <Route path='/projects/:projectName' element={ <ProjectPage projects={projects} />} />
           </Routes>
         
       </main>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,26 @@
-import ProjectGrid from "../../components/ProjectGrid"
+import ProjectCard from "../../components/ProjectCard"
+import { useEffect } from "react"
+import { useProjects } from "../../context/ProjectContext"
 
-export default function HomePage({userData, projects}) {
+export default function HomePage({userData}) {
+  
+  const { projects, getProjects } = useProjects()
+  
+  console.log(projects)
+useEffect(() => {
+  getProjects()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [])
+
   return (
     <>
     <div>HomePage</div>
     <p>{userData.login}{userData.html_url}</p>
     <div className='cardHolder'>
-        <ProjectGrid projects={projects} userData={userData} />
+        This is where the cards should display
+        {projects.map((project, idx) => {
+          return <ProjectCard project={project} key={idx} />
+        })}
         </div>
     </>
   )
