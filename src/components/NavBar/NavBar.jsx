021c055498a9eea7ba2d441af3368a35c3388a70
kiwicj <cam.orr93@gmail.com>
2024-02-07 11:46:08 +0000

    sign in/out button location & cookies

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,24 +2,32 @@ import { Link } from 'react-router-dom'
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import './NavBar.css'
 
-export default function NavBar() {
+
+export default function NavBar({ loggedIn, gitHubLogin, handleLogout, userData}) {
+
   return (
     <Navbar sticky="top" bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand as={Link} to='/'>CCC(logo?)</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/about'>About</Nav.Link>
+            <Nav.Link as={Link} to='/'>Home</Nav.Link> 
             <Nav.Link as={Link} to='/cohort'>Cohort</Nav.Link>
             <Nav.Link as={Link} to='/profilepage'>My Page</Nav.Link>
-            <Nav.Link as={Link} to='/'>Login</Nav.Link>
-            <NavDropdown title="Settings">
-              <NavDropdown.Item as={Link} to="/editprofilepage">Edit My Details</NavDropdown.Item>
-              <NavDropdown.Item to="#action4">Report</NavDropdown.Item>
-              <NavDropdown.Item to="#action4">Something Else</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item to="#action5">Log Out</NavDropdown.Item>
-            </NavDropdown>
+              <NavDropdown title="Settings">
+                {loggedIn ? (
+                <>
+                <NavDropdown.Item as={Link} to='/profilepage'>{userData.login}</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/editprofilepage">Edit My Details</NavDropdown.Item>
+                  {/* <NavDropdown.Item to="#action4">Report</NavDropdown.Item> */}
+                  {/* <NavDropdown.Item to="#action4">Something Else</NavDropdown.Item> */}
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={handleLogout}>Log Out</NavDropdown.Item>
+                </>
+                ) : (
+                  <NavDropdown.Item onClick={gitHubLogin}>Log In</NavDropdown.Item>
+                )}
+                <NavDropdown.Item as={Link} to='/about'>About</NavDropdown.Item>
+              </NavDropdown>
           </Nav>
         </Container>
     </Navbar>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,11 @@ import ProjectGrid from "../../components/ProjectGrid"
 export default function HomePage({userData, projects}) {
   return (
     <>
-    <div>HomePage</div>
-    <p>{userData.login}{userData.html_url}</p>
+    <h2>HomePage</h2>
+    <p>
+      {userData.login} <br /> 
+      {userData.html_url}
+    </p>
     <div className='cardHolder'>
         <ProjectGrid projects={projects} userData={userData} />
         </div>
