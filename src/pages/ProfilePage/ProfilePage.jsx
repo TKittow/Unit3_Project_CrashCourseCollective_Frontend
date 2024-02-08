1757d02e5686f57e5e59e78759c1e09b4ddfe0c1
joelleli <joelle.licho@rocketmail.com>
2024-02-08 14:40:36 +0000

    thurs 1

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -8,6 +8,11 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import './ProfilePage.css'
 import ProjectCard from '../../components/ProjectCard'
+import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+
+
  
 export default function ProfilePage({ userData }){
   const { getUserDetails, userDetails, userDetailsF, cohorts } = useUsers()
@@ -40,27 +45,45 @@ export default function ProfilePage({ userData }){
 let reversedUserProjects = userProjects.reverse()
 
 return (
-  <> 
-    <div className="profilePage"> 
+  <>  
+    <Container id="profileContainer"> 
     {/* <iframe title='project' src="https://joelleli.github.io/Unit1_Project_Snake/" width="400" height="400" aria-hidden="true"></iframe> */}
 
       <div id='outerInfoContainer'>
         <div id='photoContainer'>
-          <img id='profilePhoto' src={userDetails.userAvatar} alt="UserImage" className='profileImage'/>
+          <Image id='profilePhoto' src={userDetails.userAvatar} alt="UserImage" className='profileImage' roundedCircle fluid/>
         </div>
         <div id='innerInfoContainer'>
-          <div id='innerInnerInfoContainer'>
-            <h3>{userDetails.username}</h3>
-            <h6>{userDetails.fullName}</h6>
-            <h6>{userDetails.linkedIn}</h6>
-            <h6>{userDetails.gitUrl}</h6>
-            <Link to={`/cohorts/${userDetails.cohort}`}>{getCohortName(userDetails.cohort)}</Link>
+          <Card style={{ width: '70vmin' }}>
+            <Card.Body>
+              <Card.Title>{userDetails.username}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{userDetails.fullName}</Card.Subtitle>
+              <Card.Text><Link to={`/cohorts/${userDetails.cohort}`}>{getCohortName(userDetails.cohort)}</Link>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Card.Link href="#">{userDetails.linkedIn}</Card.Link>
+              <Card.Link href="#">{userDetails.gitUrl}</Card.Link>
+            </Card.Body>
+          </Card>
           </div>
-          <div id='aboutMe'>{userDetails.aboutMe}</div>
         </div>
-      </div>    
-      {/* // display public projects for non logged in users */}
-      <Button variant='primary' onClick={() => setShowModal(true)}>Add Project</Button>
+      <Card>
+            <Card.Header>About Me</Card.Header>
+            <Card.Body>
+              <blockquote id='aboutMe'>
+                <p>
+                  {' '}{userDetails.aboutMe}{' '}
+                </p>
+                {/* <footer className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </footer> */}
+              </blockquote>
+            </Card.Body>
+          </Card>   
+    </Container>
+    {/* // display public projects for non logged in users */}
+    <Button variant='primary' onClick={() => setShowModal(true)}>Add Project</Button>
       <AddProjectModal show={showModal} handleClose={handleClose} userData={userData}/>
       {/* Where the ProjectCard was */}
       <div className='cardHolder'>
@@ -68,7 +91,6 @@ return (
             return <ProjectCard project={project} key={idx} />
         })} 
       </div>
-    </div>
     </>
 )
 
